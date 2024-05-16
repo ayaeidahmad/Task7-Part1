b4b21a5369448a7f101c69bb93fa0a641c1964f8
ayaeidahmad <aya.eid.ahmad.gmail.com>
2024-05-16 00:52:07 -0700

    update

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ result_max_min.addEventListener("click",()=> {
 })
 
 // Find vowel chars 
+
 let input = document.querySelector("#num")
 let result_vowel_char = document.querySelector(".result-vowel-char")
 
 result_vowel_char.addEventListener("click",()=> {
-    // let new_input = input.value.toLowerCase()
     if (input.value.toLowerCase()=='a' || input.value.toLowerCase()=='e' || input.value.toLowerCase()=='o' || input.value.toLowerCase()=='u' || input.value.toLowerCase()=='i')
         {
             console.log("The char is : vowel" )
@@ -29,6 +29,7 @@ result_vowel_char.addEventListener("click",()=> {
 })
 
 // Print all even numbers between 1 to this number 
+
 let number = document.querySelector("#number")
 let result_even = document.querySelector(".result-even")
 
@@ -42,6 +43,7 @@ result_even.addEventListener("click",()=> {
 })
 
 // print a multiplication table up to 12
+
 let number1 = document.querySelector("#number1")
 let result_multi = document.querySelector(".result-multi")
 let Aray = [1,2,3,4,5,6,7,8,9,10,11,12]
@@ -53,7 +55,25 @@ result_multi.addEventListener("click",()=> {
         }
 })
 
+// program to enter marks of five subjects and calculate total, average and percentage.
+
+let sub1 = document.querySelector("#sub1")
+let sub2 = document.querySelector("#sub2")
+let sub3 = document.querySelector("#sub3")
+let sub4 = document.querySelector("#sub4")
+let sub5 = document.querySelector("#sub5")
+let result_marks = document.querySelector(".result-marks")
+
+result_marks.addEventListener("click",()=> {
+    let average = (parseFloat(sub1.value) + parseFloat(sub2.value) + parseFloat(sub3.value) + parseFloat(sub4.value) + parseFloat(sub5.value)) / 5
+    let sum = parseFloat(sub1.value) + parseFloat(sub2.value) + parseFloat(sub3.value) + parseFloat(sub4.value) + parseFloat(sub5.value)
+    console.log(`total is : ${sum}`)
+    console.log(`average is : ${average}`)
+    console.log(`percentage is : ${average}%`)
+})
+
 // program to input marks of five subjects to Find percentage and grade 
+
 let subject1 = document.querySelector("#subject1")
 let subject2 = document.querySelector("#subject2")
 let subject3 = document.querySelector("#subject3")
@@ -62,7 +82,7 @@ let subject5 = document.querySelector("#subject5")
 let result_grade = document.querySelector(".result-grade")
 
 result_grade.addEventListener("click",()=> {
-    let sum = (parseInt(subject1.value) + parseInt(subject2.value) + parseInt(subject3.value) + parseInt(subject4.value) + parseInt(subject5.value)) / 5
+    let sum = (parseFloat(subject1.value) + parseFloat(subject2.value) + parseFloat(subject3.value) + parseFloat(subject4.value) + parseFloat(subject5.value)) / 5
     if (sum >= 90) {
         console.log(`Percentage = ${sum}% : Grad is A`)
     }
